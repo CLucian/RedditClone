985b7076c02be011c290285e06d9e3e200eb08d4
Lucian <lucian.covic@gmail.com>
2020-11-20 18:26:59 -0500

    completed subscribed subreddits page

--- src/components/profile/SubredditList.jsx
+++ src/components/profile/SubredditList.jsx
@@ -13,58 +13,44 @@ class SubredditList extends React.Component {
             header_title,
             banner_background_color,
             banner_background_image,
+            community_icon,
+            icon_img,
         } = this.props.data.data
         console.log('subredditList', this.props.data)
 
         return (
-            <div className="master-container">
+            <div className="subreddit-master-container">
                 <div
-                    className="profile-post-container"
-                    style={{
-                        backgroundImage: `url(${
-                            banner_background_image.split('?width')[0]
-                        })`,
-                    }}
+                    className="subreddit-item-container"
                     // onClick={this.openModal}
                 >
-                    <div
-                        className="main-text-container"
-                        style={{
-                            backgroundColor: `${banner_background_color}`,
-                        }}
-                    >
+                    <div className="subreddit-header">
+                        <div className="subreddit-img-header-container">
+                            <img
+                                className="subreddit-img-header"
+                                src={
+                                    community_icon.split('?width')[0] ||
+                                    icon_img ||
+                                    header_img ||
+                                    `https://styles.redditmedia.com/t5_vm1db/styles/communityIcon_5nthugyr0ef21.png?width=256&s=3a163f7135b93df0dab0921dba35f760baea5945`
+                                }
+                            />
+                        </div>
+                        <div className="subreddit-text-container">
+                            <div className="subreddit-list-title">
+                                {display_name}
+                            </div>
+                            <div className="subreddit-description">
+                                {public_description}
+                            </div>
+                        </div>
+                    </div>
+                    {/* <div className="main-text-container">
                         <div className="subreddit-banner">
                             {/* <img
                                 className="banner-img"
                                 src={banner_background_image.split('?width')[0]}
                             /> */}
-                        </div>
-                        <div className="subreddit-header">
-                            <div className="subreddit-header-titles">
-                                <div className="subreddit-title">
-                                    {display_name}
-                                </div>
-                                <div className="subreddit-header-line">
-                                    "{header_title}"
-                                </div>
-                            </div>
-
-                            <div className="subreddit-img-header-container">
-                                <img
-                                    className="subreddit-img-header"
-                                    src={header_img}
-                                />
-                            </div>
-                        </div>
-                        <div className="subreddit-description">
-                            {public_description}
-                        </div>
-
-                        <div className="post-description"></div>
-                        <div className="hr-container">
-                            <hr className="post-hr"></hr>
-                        </div>
-                    </div>
                 </div>
             </div>
         )
@@ -72,3 +58,25 @@ class SubredditList extends React.Component {
 }
 
 export default SubredditList
+
+//  </div>
+//                         <div className="subreddit-header">
+//                             <div className="subreddit-img-header-container">
+//                                 <img
+//                                     className="subreddit-img-header"
+//                                     src={header_img}
+//                                 />
+//                             </div>
+//                             <div className="subreddit-header-titles">
+//                                 <div className="subreddit-title">
+//                                     {display_name}
+//                                 </div>
+//                                 <div className="subreddit-header-line">
+//                                     "{header_title}"
+//                                 </div>
+//                             </div>
+//                         </div>
+//                         <div className="subreddit-description">
+//                             {public_description}
+//                         </div>
+//                     </div>
